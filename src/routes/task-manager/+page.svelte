c3e3f0a1e550c3bad8b786668e2bc3258c5a93c2
navin-triad <navin.sadarangani@triad.co.uk>
2025-04-08 22:07:52 +0100

    condition on reactive statement

--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -4,20 +4,23 @@
 	//reactive statement
 	//executes whenever dependancy is changed, in this case variable count
 	$: {
-		console.log(count);
+		if (count > 10) {
+			console.log('I am winner, now that it is ' + count);
+		}
 	}
 
-    //reactive declaration
-    $: doubled = count * 2;
+	//reactive declaration
+	$: doubled = count * 2;
 </script>
 
 <div class="text-white">
 	<div>
 		I am a task manager page - {count}
 	</div>
-    <div>Doubled value: {doubled}</div>
+	<div>Doubled value: {doubled}</div>
 	<button
 		on:click={() => {
 			count++;
-		}}>Increment</button>
+		}}> Increment
+    </button>
 </div>
