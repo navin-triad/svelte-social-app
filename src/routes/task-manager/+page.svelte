476a918d801a6810f4ebd1887cf07436127ab2dd
navin-triad <navin.sadarangani@triad.co.uk>
2025-04-08 22:00:56 +0100

    reactive statement and delcaration

--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -1,4 +1,23 @@
+<script>
+	let count = 0;
+
+	//reactive statement
+	//executes whenever dependancy is changed, in this case variable count
+	$: {
+		console.log(count);
+	}
+
+    //reactive declaration
+    $: doubled = count * 2;
+</script>
 
 <div class="text-white">
-    I am a task manager page
+	<div>
+		I am a task manager page - {count}
+	</div>
+    <div>Doubled value: {doubled}</div>
+	<button
+		on:click={() => {
+			count++;
+		}}>Increment</button>
 </div>
