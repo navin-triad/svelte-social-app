148d4f56e42dd4dee60fe36c6b264c27940ca845
navin-triad <navin.sadarangani@triad.co.uk>
2025-05-10 11:02:08 +0100

    for each loop syntax

--- src/components/task-manager/TaskList.svelte
+++ src/components/task-manager/TaskList.svelte
@@ -1,12 +1,13 @@
 <script>
 	import TaskItem from "./TaskItem.svelte";
-    export let ListName;
+    export let listName;
+    export let tasks;
 </script>
 <div class="flex-it h-full w-80 max-w-sm min-h-full m-2 my-0">
     <div class="bg-slate-400 flex-it rounded-xl max-h-full border-2 border-gray-500">
         <div class="flex-it m-3">
             <div class="flex-it flex-row">
-                <div class="text-xl text-left font-bold mr-2">{ListName}</div>
+                <div class="text-xl text-left font-bold mr-2">{listName}</div>
                 <div class="flex hover:text-red-600 items-center">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -28,10 +29,9 @@
             </div>
         </div>
         <div class="overflow-x-hidden overflow-y-auto with-scrollbar p-2">
-            <TaskItem taskDescription="todo 1"></TaskItem>
-            <TaskItem></TaskItem>
-            <TaskItem ></TaskItem>
-            <TaskItem />
+            {#each tasks as task(task.id)}
+                <TaskItem taskDescription="{task.text}"></TaskItem>
+            {/each}
         </div>
         <button class="underline flex p-2"> + Add Task </button>
     </div>
--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -1,29 +1,60 @@
 <script>
-		import TaskList from "../../components/task-manager/TaskList.svelte";
-		const list1 = "First";
+	import TaskList from '../../components/task-manager/TaskList.svelte';
 
-		const taskList = [
-    {id: "l-1", text: "List 1", items: [{id: "t-1", text: "Task 1"},{id: "t-2", text: "Task 2"},{id: "t-3", text: "Task 3"}]},
-    {id: "l-2", text: "List 2", items: [{id: "t-4", text: "Task 4"},{id: "t-5", text: "Task 5"},{id: "t-6", text: "Task 6"}]},
-    {id: "l-3", text: "List 3", items: [{id: "t-7", text: "Task 7"},{id: "t-8", text: "Task 8"},{id: "t-9", text: "Task 9"}]},
-  ];
+	const taskList = [
+		{
+			id: 'l-1',
+			text: 'List 1',
+			items: [
+				{ id: 't-1', text: 'Task 1' },
+				{ id: 't-2', text: 'Task 2' },
+				{ id: 't-3', text: 'Task 3' }
+			]
+		},
+		{
+			id: 'l-2',
+			text: 'List 2',
+			items: [
+				{ id: 't-4', text: 'Task 4' },
+				{ id: 't-5', text: 'Task 5' },
+				{ id: 't-6', text: 'Task 6' }
+			]
+		},
+		{
+			id: 'l-3',
+			text: 'List 3',
+			items: [
+				{ id: 't-7', text: 'Task 7' },
+				{ id: 't-8', text: 'Task 8' },
+				{ id: 't-9', text: 'Task 9' }
+			]
+		}
+	];
+
+	let count = 0;
+	//reactive statement
+	//executes whenever dependancy is changed, in this case variable count
+	$: {
+		console.log(count);
+	}
+	//reactive declaration
+	$: doubled = count * 2;
 </script>
 
-<div class="text-white">
+<!-- <div class="text-white">
   {JSON.stringify(taskList)}
-</div>
+</div> -->
 
 <div class="p-10 h-full">
-		<div class="text-white text-2xl mb-6">Some List</div>
-		<button class="text-xl mb-3 text-white font-bold cursor-pointer hover:underline flex items-start">
-				+ Add List
-		</button>
-		<div class="flex-it h-full">
-				<div class="flex-it flex-row rounded-xl h-full">
-						<TaskList ListName="{list1}" />
-						<TaskList ListName="{"Two after " + list1}" />
-						<TaskList ListName="Three" />
-						<TaskList ListName={`Four after ${list1}`} /> <!--interpolating-->
-				</div>
+	<div class="text-white text-2xl mb-6">Some List</div>
+	<button class="text-xl mb-3 text-white font-bold cursor-pointer hover:underline flex items-start">
+		+ Add List
+	</button>
+	<div class="flex-it h-full">
+		<div class="flex-it flex-row rounded-xl h-full">
+			{#each taskList as list (list.id)}
+				<TaskList listName={list.text} tasks={list.items} />
+			{/each}
 		</div>
+	</div>
 </div>
